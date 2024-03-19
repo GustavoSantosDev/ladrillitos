e742c602e2c785a22a639690f547dc463be2f021
GustavoSantosDev <jgustavo.santosc@gmail.com>
2024-03-18 18:53:50 -0600

    favicon and tittle

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
